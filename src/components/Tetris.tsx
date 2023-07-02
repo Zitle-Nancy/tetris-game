b973ee84be9f5aaa2bf047548013a77d732e99ac
Nancy Zitle Juárez <nancy-zitle@Nancys-MacBook-Pro.local>
2023-07-01 22:51:06 -0700

    feat(Tetris): import useStage Hook and create dropTime and gameOver states

--- src/components/Tetris.tsx
+++ src/components/Tetris.tsx
@@ -1,19 +1,32 @@
-import { createStage } from "../gameHelpers";
+import { useState } from "react";
 import { Display, Stage, StartButton } from "./index";
 import { StyledTetris, StyledTetrisWrapper } from "./styles";
+import usePlayer from "../hooks/usePlayer";
+import useStage from "../hooks/useStage";
 
 // { callback }
 const Tetris = () => {
+  // @dropTime is about game speed
+  const [dropTime, setDropTime] = useState(null);
+  const [gameOver, setGameOver] = useState(false);
+
+  const [player] = usePlayer();
+  const [stage, setStage] = useStage();
+
   return (
     <StyledTetrisWrapper>
       <StyledTetris>
-        <Stage stage={createStage()} />
+        <Stage stage={stage as (string | number)[][][]} />
         <aside>
-          <div>
-            <Display text="Score" gameOver={""} />
-            <Display text="Rows" gameOver={""} />
-            <Display text="Level" gameOver={""} />
-          </div>
+          {gameOver ? (
+            <Display text="Score" gameOver="Game Over" />
+          ) : (
+            <div>
+              <Display text="Score" />
+              <Display text="Rows" />
+              <Display text="Level" />
+            </div>
+          )}
           <StartButton />
         </aside>
       </StyledTetris>
