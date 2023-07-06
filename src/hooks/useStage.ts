4018c35e2826f1ce737d3cfda7f040659b8f3b37
Nancy Zitle Juárez <nancy-zitle@Nancys-MacBook-Pro.local>
2023-07-05 21:55:18 -0700

    feat(useStage.ts): use object destructuring instead of array destructuring

--- src/hooks/useStage.ts
+++ src/hooks/useStage.ts
@@ -1,10 +1,10 @@
-import { Dispatch, SetStateAction, useState } from "react";
+import { useState } from "react";
 import { createStage } from "../gameHelpers";
 
 const useStage = () => {
   const [stage, setStage] = useState<(string | number)[][][]>(createStage());
 
-  return [stage, setStage];
+  return {stage, setStage};
 }
 
 export default useStage;
