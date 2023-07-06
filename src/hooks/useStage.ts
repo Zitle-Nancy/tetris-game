39f22372553807728d2a3b7eaccd462b9095d518
Nancy Zitle Juárez <nancy-zitle@Nancys-MacBook-Pro.local>
2023-07-05 22:52:33 -0700

    WIP(useStage): add functions

--- src/hooks/useStage.ts
+++ src/hooks/useStage.ts
@@ -1,8 +1,45 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { createStage } from "../gameHelpers";
 
-const useStage = () => {
+interface IPos {
+  x: number;
+  y: number;
+}
+interface IStage {
+  player: {
+    pos: IPos,
+    tetromino: (string | number)[][],
+    collided: boolean
+  };
+  resetPlayer: () => void;
+}
+const useStage  = (props: IStage ) => {
+  const {player, resetPlayer} = props;
   const [stage, setStage] = useState<(string | number)[][][]>(createStage());
+
+  useEffect(() => {
+    const updateStage = (prevStage: any) => {
+      // first flush the stage
+      const newStage = prevStage.map((row: any) => 
+        row.map((cell: any) => (cell[1] === 'clear' ? [0, 'clear'] : cell))
+      )
+
+      // Then draw the tetrominio
+      player.tetromino.forEach((row, y) => {
+        row.forEach((value, x) => {
+          if(value !== 0){
+            newStage[y + player.pos.y][x + player.pos.x] = [
+              value,
+              `${player.collided ? 'merged' : 'clear'}`
+            ]
+          }
+        })
+      });
+      return newStage;
+    }
+
+    setStage(prev => updateStage(prev))
+  },[player])
 
   return {stage, setStage};
 }
